4c49f726e56e85bf15a3954f1d696d9385916199
Rob <rob@darwin.finance>
2022-03-01 12:50:09 -0500

    Add trade feed to futures

--- v2/futures/websocket_service.go
+++ v2/futures/websocket_service.go
@@ -908,3 +908,37 @@ func WsUserDataServe(listenKey string, handler WsUserDataHandler, errHandler Err
 	}
 	return wsServe(cfg, wsHandler, errHandler)
 }
+
+// WsTradeHandler handle websocket trade event
+type WsTradeHandler func(event *WsTradeEvent)
+
+// WsTradeServe serve websocket handler with a symbol
+func WsTradeServe(symbol string, handler WsTradeHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
+	endpoint := fmt.Sprintf("%s/%s@trade", getWsEndpoint(), strings.ToLower(symbol))
+	cfg := newWsConfig(endpoint)
+	wsHandler := func(message []byte) {
+		event := new(WsTradeEvent)
+		err := json.Unmarshal(message, event)
+		if err != nil {
+			errHandler(err)
+			return
+		}
+		handler(event)
+	}
+	return wsServe(cfg, wsHandler, errHandler)
+}
+
+// WsTradeEvent define websocket trade event
+type WsTradeEvent struct {
+	Event         string `json:"e"`
+	Time          int64  `json:"E"`
+	Symbol        string `json:"s"`
+	TradeID       int64  `json:"t"`
+	Price         string `json:"p"`
+	Quantity      string `json:"q"`
+	BuyerOrderID  int64  `json:"b"`
+	SellerOrderID int64  `json:"a"`
+	TradeTime     int64  `json:"T"`
+	IsBuyerMaker  bool   `json:"m"`
+	Placeholder   bool   `json:"M"` // add this field to avoid case insensitive unmarshaling
+}
